99140dc709a9f7b6c40b925788432fd60b524350
Ariel Solomon <ariel.solomon@gmail.com>
2023-12-17 15:27:24 +0200

    Add files via upload

--- clip_distl_with_labeled.py
+++ clip_distl_with_labeled.py
@@ -1,4 +1,7 @@
-from stl10_utils import precompute_clip_stl10_train_image_embeddings, precompute_clip_stl10_test_image_embeddings, precompute_clip_stl10_text_embeddings, train_resnet18_from_scratch, train_resnet18_zero_shot_train_only, train_resnet18_linear_probe_train_only
+from cifar10_c_utils import (precompute_clip_stl10_train_image_embeddings,
+                         precompute_clip_stl10_test_image_embeddings,
+                         precompute_clip_stl10_text_embeddings, train_resnet18_from_scratch,
+                         train_resnet18_zero_shot_train_only, train_resnet18_linear_probe_train_only)
 
 def f_name(f):
     return f.__name__
