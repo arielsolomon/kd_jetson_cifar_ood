b62fe882f83b607c0c8bde652e9f505f35ce9296
Ariel Solomon <ariel.solomon@gmail.com>
2023-12-27 15:58:44 +0200

    Add files via upload

--- clip_distil_with_unlabled.py
+++ clip_distil_with_unlabled.py
@@ -1,4 +1,6 @@
-from cifar10_c_unlabled_utils  import (
+import warnings
+import os
+from cifar10_c_unlabeled_utils  import (
     precompute_clip_stl10_train_image_embeddings,
     precompute_clip_stl10_unlabeled_image_embeddings,
     precompute_clip_stl10_test_image_embeddings,
@@ -9,11 +11,26 @@ from cifar10_c_unlabled_utils  import (
 
 def f_name(f):
     return f.__name__
+def make_dir(root, target):
+    if not os.path.exists(root + target):
+        os.mkdir(root + target)
+warnings.filterwarnings('ignore')
+root = '/home/user1/ariel/federated_learning/kd_jetson_cifar_ood/'
+make_dir(root, 'data/')
+make_dir(root, 'data/cifar10_c_unlabeled/')
+make_dir(root,'data/experiments/')
+make_dir(root,'data/experiments/cifar10_c_unlabeled')
+
 
 precompute_clip_stl10_train_image_embeddings()
+print('\nFinished precompute_clip_stl10_train_image_embeddings())')
 precompute_clip_stl10_unlabeled_image_embeddings()
+print('\nFinished precompute_clip_stl10_unlabeled_image_embeddings()')
 precompute_clip_stl10_test_image_embeddings()
+print('\nFinished precompute_clip_stl10_test_image_embeddings()')
 precompute_clip_stl10_text_embeddings()
-train_resnet18_zero_shot(f_name(train_resnet18_zero_shot))
+print('\nFinished precompute_clip_stl10_text_embeddings()')
 train_resnet18_linear_probe(f_name(train_resnet18_linear_probe))
-
+print('\nFinished train_resnet18_linear_probe(f_name(train_resnet18_linear_probe))')
+train_resnet18_zero_shot(f_name(train_resnet18_zero_shot))
+print('\nFinished train_resnet18_zero_shot(f_name(train_resnet18_zero_shot))')
--- clip_distl_with_labeled.py
+++ clip_distl_with_labeled.py
@@ -1,3 +1,5 @@
+import os
+import warnings
 from cifar10_c_utils import (precompute_clip_stl10_train_image_embeddings,
                          precompute_clip_stl10_test_image_embeddings,
                          precompute_clip_stl10_text_embeddings, train_resnet18_from_scratch,
@@ -5,10 +7,24 @@ from cifar10_c_utils import (precompute_clip_stl10_train_image_embeddings,
 
 def f_name(f):
     return f.__name__
-
-precompute_clip_stl10_train_image_embeddings()
+# def make_dir(root, target):
+#     if not os.path.exists(root + target):
+#         os.mkdir(root + target)
+# warnings.filterwarnings('ignore')
+# root = '/home/user1/ariel/federated_learning/kd_jetson_cifar_ood/'
+# make_dir(root, 'data/')
+# make_dir(root, 'data/cifar10_c_jpeg_compression/')
+# make_dir(root,'data/experiments/')
+# make_dir(root,'data/experiments/cifar10_c_jpeg_compression')
+# precompute_clip_stl10_train_image_embeddings()
+print('\nFinished precompute_clip_stl10_train_image_embeddings')
 precompute_clip_stl10_test_image_embeddings()
+print('\nFinished precompute_clip_stl10_test_image_embeddings')
 precompute_clip_stl10_text_embeddings()
+print('\nFinished precompute_clip_stl10_text_embeddings')
 train_resnet18_from_scratch()
+print('\nFinished train_resnet18_from_scratch')
 train_resnet18_zero_shot_train_only(f_name(train_resnet18_zero_shot_train_only))
+print('\nFinished train_resnet18_zero_shot_train_only')
 train_resnet18_linear_probe_train_only(f_name(train_resnet18_linear_probe_train_only))
+print('\nFinished train_resnet18_linear_probe_train_only')
